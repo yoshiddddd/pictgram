7b5beaec29515c1abab9364cd0ea36c7087b4cde
吉田和司 <yoshidakerapo@gmail.com>
2021-06-19 05:39:21 +0000

    kadai3

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
      validates :name, presence: true, length: {maximum:15}
   validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   
-  validates :password, presence: true, length: {in:8..32} ,format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]\z/i}
+  validates :password, presence: true, length: {in:8..32} ,format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
   has_secure_password
+   has_many :topics
+   has_many :favorites
 end
